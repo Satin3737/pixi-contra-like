fe4065c9b7440aabf1b355ca3b975454a8f0d90b
satin <satin3737@gmail.com>
2026-03-08 22:10:13 +0200

    Small changes for the platform class

--- src/entities/Platforms/Platform.ts
+++ src/entities/Platforms/Platform.ts
@@ -11,17 +11,15 @@ class Platform extends Container {
         super(options);
 
         this.type = type;
-        this.setSize(size);
-
         this.drawPlatform(size);
-        this.addChild(this.view);
-
         this.isSteppable = this.type === PlatformTypes.solid && this.height >= PlatformSteppableHeight;
     }
 
     private drawPlatform({width, height}: ISize): void {
-        this.view.rect(0, 0, width, height).stroke({width: 2, color: 0x00ff00});
-        this.type === PlatformTypes.solid && this.view.lineTo(width, height).stroke({width: 2, color: 0x00ff00});
+        this.view.setSize({width, height});
+        this.view.rect(0, 0, width, height).stroke({width: 1, color: 0x00ff00});
+        this.type === PlatformTypes.solid && this.view.lineTo(width, height).stroke({width: 1, color: 0x00ff00});
+        this.addChild(this.view);
     }
 }
 
