729af2fd82e3c94fbdc95efee801440a7753463c
satin <satin3737@gmail.com>
2026-03-25 22:05:22 +0200

    Add simple enemies data

---
+++ src/data/enemies.ts
@@ -0,0 +1,7 @@
+import type {IRunner} from '@/entities/enemies/Runner';
+
+export const EnemiesData: IRunner[] = [
+    {x: 1000, y: 320},
+    {x: 1200, y: 470},
+    {x: 800, y: 470}
+];
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,2 @@
+export * from './enemies';
 export * from './platforms';
