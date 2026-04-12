01385cae001e0e8e57b8d385381387b065382067
satin <satin3737@gmail.com>
2026-04-12 21:16:51 +0300

    Set up game container and adjust application initialization for better layout

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import {initDevtools} from '@pixi/devtools';
 import {Application} from 'pixi.js';
 import '@/assets/styles/general.scss';
-import Game from '@/Game';
+import Game from '@/Core/Game';
+
+const gameContainer = document.querySelector<HTMLDivElement>('#game');
+if (!gameContainer) throw new Error('Game container not found');
 
 const app = new Application();
-await app.init({resizeTo: window});
+await app.init({resizeTo: gameContainer, width: 1024, height: 768});
+
 await initDevtools({app});
-document.body.appendChild(app.canvas);
+gameContainer.appendChild(app.canvas);
 
 new Game(app);
