c221ba11f67f8ce82f48979f993bb6977f6f5073
satin <satin3737@gmail.com>
2026-04-16 01:03:16 +0300

    Update enemy spawn positions to use PlatformDefaultWidth for consistency

--- src/data/enemies.ts
+++ src/data/enemies.ts
@@ -1,9 +1,26 @@
-import type {IEntityCommonParams} from '@/entities';
+import {type IEntityCommonParams, PlatformDefaultWidth} from '@/entities';
 
 export const RunnersData: IEntityCommonParams[] = [
-    {options: {x: 900, y: 320}},
-    {options: {x: 1000, y: 470}},
-    {options: {x: 800, y: 470}}
+    {options: {x: PlatformDefaultWidth * 9, y: 290}},
+    {options: {x: PlatformDefaultWidth * 10, y: 290}},
+    {options: {x: PlatformDefaultWidth * 11, y: 290}},
+    {options: {x: PlatformDefaultWidth * 13, y: 290}},
+    {options: {x: PlatformDefaultWidth * 14, y: 290}},
+    {options: {x: PlatformDefaultWidth * 15, y: 290}},
+    {options: {x: PlatformDefaultWidth * 16, y: 290}},
+    {options: {x: PlatformDefaultWidth * 20, y: 290}},
+    {options: {x: PlatformDefaultWidth * 21, y: 290}},
+    {options: {x: PlatformDefaultWidth * 29, y: 290}},
+    {options: {x: PlatformDefaultWidth * 30, y: 290}},
+    {options: {x: PlatformDefaultWidth * 40, y: 400}},
+    {options: {x: PlatformDefaultWidth * 42, y: 400}}
 ];
 
-export const TurretsData: (IEntityCommonParams & {health: number})[] = [{health: 5, options: {x: 500, y: 200}}];
+export const TurretsData: (IEntityCommonParams & {health: number})[] = [
+    {health: 5, options: {x: PlatformDefaultWidth * 10, y: 670}},
+    {health: 5, options: {x: PlatformDefaultWidth * 22, y: 500}},
+    {health: 5, options: {x: PlatformDefaultWidth * 29, y: 550}},
+    {health: 5, options: {x: PlatformDefaultWidth * 35, y: 550}},
+    {health: 5, options: {x: PlatformDefaultWidth * 45, y: 670}},
+    {health: 5, options: {x: PlatformDefaultWidth * 48, y: 670}}
+];
